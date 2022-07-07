8acd09a667199484c88b24e4c93da5e226b7d748
DTurner94 <turnerdandre23@gmail.com>
2022-07-07 13:50:45 -0400

    variables

--- variablePractice.js
+++ variablePractice.js
@@ -10,12 +10,21 @@ console.log(num)
 
 //--- Medium
 //create a variable that holds a value from the input
+let inputedVal = document.querySelector('#danceDanceRevolution').value
 
 //add 25 to that number
+inputedVal = inputedVal + 25
 
 //alert that number
+alert(inputedVal)
 
 //--- Hard
 //create a variable that holds the h1
-
+const h1Holder = document.querySelector('h1')
 //add an event listener to that element that console logs the sum of the two previous variables
+
+h1Holder.addEventListener('click', sum)
+
+function sum(){
+    console.log(num + Number(inputedVal))
+}
